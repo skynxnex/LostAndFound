80a344a5163522b252dd10dbefb02e5cedc837a2
lickan <Linda@.(none)>
2011-02-16 13:21:59 +0100

    Put list in sidebar.php

--- script.js
+++ script.js
@@ -8,4 +8,4 @@ onload = function(){
 
 
 
-}
+};
--- sidebar.php
+++ sidebar.php
@@ -1,5 +1,77 @@
 <div id="sidebar">
-	<h4>Latest items</h4>
-	<p>Här ligger de senaste 10 lostnfound.</p>
+	<h4>Latest added</h4>
+		<ul id="itemList">
+			<li>
+				<img class="thumbnail" src="peakcykel.jpg" />
+				<ul id="item1">
+					<li class="what"><a href="">Vad:</a></li>
+					<li class="when"><a href="">När:</a></li>
+					<li class="where"><a href="">Var:</a></li>
+				</ul>
+			</li>
+			<li><img class="thumbnail" src="sony-ericsson-s500i.jpg" />
+				<ul id="item2">
+					<li class="what"><a href="">Vad:</a></li>
+					<li class="when"><a href="">När:</a></li>
+					<li class="where"><a href="">Var:</a></li>
+				</ul>
+			</li>
+			<li><img class="thumbnail" src="peakcykel.jpg" />
+				<ul id="item3">
+					<li class="what"><a href="">Vad:</a></li>
+					<li class="when"><a href="">När:</a></li>
+					<li class="where"><a href="">Var:</a></li>
+				</ul>
+			</li>
+			<li><img class="thumbnail" src="sony-ericsson-s500i.jpg" />
+				<ul id="item4">
+					<li class="what"><a href="">Vad:</a></li>
+					<li class="when"><a href="">När:</a></li>
+					<li class="where"><a href="">Var:</a></li>
+				</ul>
+			</li>
+			<li><img class="thumbnail" src="peakcykel.jpg" />
+				<ul id="item5">
+					<li class="what"><a href="">Vad:</a></li>
+					<li class="when"><a href="">När:</a></li>
+					<li class="where"><a href="">Var:</a></li>
+				</ul>
+			</li>
+			<li><img class="thumbnail" src="sony-ericsson-s500i.jpg" />
+				<ul id="item6">
+					<li class="what"><a href="">Vad:</a></li>
+					<li class="when"><a href="">När:</a></li>
+					<li class="where"><a href="">Var:</a></li>
+				</ul>
+			</li>
+			<li><img class="thumbnail" src="peakcykel.jpg" />
+				<ul id="item7">
+					<li class="what"><a href="">Vad:</a></li>
+					<li class="when"><a href="">När:</a></li>
+					<li class="where"><a href="">Var:</a></li>
+				</ul>
+			</li>
+			<li><img class="thumbnail" src="sony-ericsson-s500i.jpg" />
+				<ul id="item8">
+					<li class="what"><a href="">Vad:</a></li>
+					<li class="when"><a href="">När:</a></li>
+					<li class="where"><a href="">Var:</a></li>
+				</ul>
+			</li>
+			<li><img class="thumbnail" src="peakcykel.jpg" />
+				<ul id="item9">
+					<li class="what"><a href="">Vad:</a></li>
+					<li class="when"><a href="">När:</a></li>
+					<li class="where"><a href="">Var:</a></li>
+				</ul>
+			</li>
+			<li><img class="thumbnail" src="sony-ericsson-s500i.jpg" />
+				<ul id="item10">
+					<li class="what"><a href="">Vad:</a></li>
+					<li class="when"><a href="">När:</a></li>
+					<li class="where"><a href="">Var:</a></li>
+				</ul>
+			</li>
+		</ul>
 	<h4>Ev fler headers här då</h4>
 </div>

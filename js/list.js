d2847699c46dd16a9ea07658c0fe894464303d5d
adrianforsius <adrianforsius@gmail.com>
2011-02-22 17:53:02 +0100

    rock n roll med merge :)

--- js/list.js
+++ js/list.js
@@ -19,11 +19,50 @@ $(function() {
 				$("#title").empty();
 				$("<h2/>").text(ad.lost_found+" : "+ad.title).appendTo("#title");
 				$("#item_photo").empty();
-				$("<img src='"+ad.item_picture_link+"'/>").appendTo("#item_photo");
+				if(ad.item_picture_link != null){
+					$("<img src='"+ad.item_picture_link+"'/>").appendTo("#item_photo");
+				} else if(ad.where_picture_link != null){
+					$("<img src='"+ad.where_picture_link+"'/>").appendTo("#item_photo");
+				}
 				$("#item_text").empty();
-				$("<p/>").text(ad.description).appendTo("#item_text");
-				$("<p/>").text(ad.contact_comment).appendTo("#item_text");
-				
+				if(ad.description != null){
+					$("<p/>").text("Description: " + ad.description).appendTo("#item_text");
+				}
+				if(ad.datetime != null){
+					$("<p/>").text("Found/lost: " + ad.datetime).appendTo("#item_text");
+				} else {
+					$("<p/>").text("Posted: " + ad.timestamp).appendTo("#item_text");
+				}
+				if(ad.when_comment != null){
+					$("<p/>").text(ad.when_comment).appendTo("#item_text");	
+				}
+				if(ad.where_street && ad.where_street_no != null){
+					$("<p/>").text("Where: " + ad.where_street + " " + ad.where_street_no).appendTo("#item_text");
+				}
+				if(ad.where_city != null){
+					$("<p/>").text(ad.where_city).appendTo("#item_text");
+				}
+				if(ad.where_comment != null){
+					$("<p/>").text(ad.where_comment).appendTo("#item_text");
+				}
+				$("#item_contact").empty();
+				$("<h3/>").text("Contact").appendTo("#item_contact");
+				if(ad.email != null){
+					$("<p/>").text(ad.email).appendTo("#item_contact");
+				}
+				if(ad.phone != null){
+					$("<p/>").text(ad.phone).appendTo("#item_contact");
+				}
+				if(ad.contact_street && ad.contact_street_no != null){
+					$("<p/>").text(ad.contact_street + " " + ad.contact_street_no).appendTo("#item_contact");
+				}
+				if(ad.contact_city != null){
+					$("<p/>").text(ad.contact_city).appendTo("#item_contact");
+				}
+				if(ad.contact_comment != null){
+					$("<p/>").text(ad.contact_comment).appendTo("#item_contact");
+				}
+					
 			});
 		});
 	};
